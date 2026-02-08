d65066ab65ea6c23f06532bd1c3a1aecfd54eb8c
Elaf <elaf88f@gmail.com>
2026-02-08 15:19:03 +0300

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,37 @@
 import 'package:flutter/material.dart';
- HEAD
-import 'splash_screen.dart';
-import 'start_screen.dart';
-import 'sign_in_screen.dart';
-import 'sign_up_screen.dart';
 
-import 'package:pulse_hear/views/bluetooth-asayel/bluetooth_search_screen.dart';
- f272aee36f2acde65ec4caeabf33588e7a071a50
+import 'views/splash-elaf/splash_screen.dart';
+import 'views/auth-elaf/start_screen.dart';
+import 'views/auth-elaf/sign_in_screen.dart';
+import 'views/auth-elaf/sign_up_screen.dart';
+
+import 'views/bluetooth-asayel/bluetooth_search_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
- HEAD
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PulseHear',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.purple, 
       ),
+
       home: const SplashScreen(),
+      
       routes: {
         '/splash': (context) => const SplashScreen(),
         '/start': (context) => const StartScreen(),
         '/signin': (context) => const SignInScreen(),
         '/signup': (context) => const SignUpScreen(),
+        '/bluetooth': (context) => const PairWristbandScreen(),
       },
     );
   }
-
- 
-
-
-@override 
-Widget build(BuildContext context) {
-  return MaterialApp(
-   
-    home: PairWristbandScreen(), debugShowCheckedModeBanner: false,
-  );
-}
->>>>>>> f272aee36f2acde65ec4caeabf33588e7a071a50
 }
